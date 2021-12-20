f0746b1da68aa499de57d9a08c2bcf06907a6216
webandrade@gmail.com <Alex Andrade>
2021-12-20 18:42:34 -0300

    Tela de Login

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,32 @@
 const myModal = new bootstrap.Modal("#register-modal");
 
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-imput").value;
+    const password = document.getElementById("password-imput").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if (!account) {
+        alert("Opss! Verifique o usuário ou a senha.");
+        return;       
+    }
+
+    if (account) {
+        if(account.password !== password) {
+            alert("Opss! Verifique o usuário ou a senha.");
+        return
+        }
+
+        saveSession(email, checkSession);
+
+        window.location.href = "home.html"
+    }
+});
+
 //Criar Conta
 document.getElementById("creat-form").addEventListener("submit", function(e) {
     e.preventDefault();
@@ -21,7 +48,6 @@ document.getElementById("creat-form").addEventListener("submit", function(e) {
         login:email,
         pasword: pasword,
         transactions: []
-
     })
 
     myModal.hide();
@@ -31,4 +57,22 @@ document.getElementById("creat-form").addEventListener("submit", function(e) {
 
 function saveAccount(data) {
     localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
+    
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account) {
+        return JSON.parse(account);
+    }
+
+    return "";
 }
