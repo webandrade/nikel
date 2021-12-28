a550cc887a6aad46434c67fa48d93ac785dfa033
webandrade <webandrade@gmail.com>
2021-12-28 00:09:33 -0300

    Botão sair

--- public/js/home.js
+++ public/js/home.js
@@ -2,10 +2,12 @@ const myModal = new bootstrap.Modal("#transaction-modal");
 const session = localStorage.getItem("session");
 let logged = sessionStorage.getItem("logged");
 let data = {
-
+    trasactions: []
 }
 
 checkedLogged();
+
+document.getElementById("button-logout").addEventListener("click", logout);
 
 function checkedLogged() {
     if (session) {
@@ -17,4 +19,16 @@ function checkedLogged() {
         window.location.href = "index.html";
         return; 
     }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    } 
+}
+
+function logout(){
+        sessionStorage.removeItem("logged");
+        localStorage.removeItem("session");
+
+        window.location.href = "index.html";
 }
